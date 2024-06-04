64df568b8b9d38d5eb87c15bfce61e141cf491a2
kamal sharma <kamalbsr2101@gmail.com>
2024-06-04 16:45:43 +0530

    add more

--- 01.basics/script.py
+++ 01.basics/script.py
@@ -1,8 +1,48 @@
-print('Hello world')
+# print('Hello world')
 
 def first(n):
     print(n);
-first('Krishna')
+# first('Krishna')
 
 chai_one = "masala chai"
 chai_two = "ginger tea"
+
+# chai_one.lower()
+
+# chai_one.upper()
+
+# chai_one.strip()   // Remove first and last white spaces
+
+# print(chai_one.replace("masala", "ginger"))  // Replace charachater
+
+# chai_one.split(", ")   // Split seperate character list
+
+# chai_two.find("tea")    // Find starting position of character
+
+# chai_two.count("tea")    // Count character repeating
+
+
+# =====****=====
+chai_type = "masala"
+quantity = 2
+order = "I have ordered {} cups {} chai"
+
+# print(order.format(quantity, chai_type))
+
+
+# =====****=====
+movies_list = ["Nagraaj", "Bahubali", "3-adiots", "artical-370"]
+
+# print(" ".join(movies_list))
+# print("-".join(movies_list))
+
+# print(len(movies_list))
+
+# for letter in movies_list: print(letter)   // This is loop 
+
+
+# ======******=======
+
+quotes = "He said, \"Artical-370 is awesome\" "
+
+# print(quotes)
